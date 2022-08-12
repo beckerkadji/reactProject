aa89de6b025ac94b0db7e46ff776b9de958711a0
beckerkadji <kadjibecker@gmail.com>
2022-08-12 18:23:24 +0100

    learn hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Blogs from "./pages/Blogs";
+import NotFound from "./pages/NotFoundPage";
+import TodoPage from "./pages/Todo";
+import "../src/styles/App.css"
 
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/Blogs" element={<Blogs />} />
+                <Route path="*" element={<NotFound />} />
+                <Route path="/todopage" element={<TodoPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
